7b123256b1550e3b90ec686cdd2d5961416d0a2f
JakeKarasik <8940940+JakeKarasik@users.noreply.github.com>
2017-10-23 19:19:58 -0400

    Add comments for layout

--- sock352.py
+++ sock352.py
@@ -8,7 +8,7 @@ import sys
 # define the UDP ports all messages are sent
 # and received from
 
-def init(UDPportTx,UDPportRx):   # initialize your UDP socket here 
+def init(UDPportTx, UDPportRx):   # initialize your UDP socket here 
     pass 
     
 class socket:
@@ -19,28 +19,39 @@ class socket:
     def bind(self, address):
         return 
 
-    def connect(self, address):  # fill in your code here 
+    def connect(self, address):  # fill in your code here
+    	# Bind
+    	# Create the SYN header
+    	# Send the SYN packet A
+    	# Start timer
+    	# recv SYN ACK B
+    	# send ACK C
+    	# if there is error, send header again
         return 
     
     def listen(self, backlog):
         return
 
     def accept(self):
-        (clientsocket, address) = (1,1)  # change this to your code 
-        return (clientsocket,address)
+    	# recv SYN A
+    	# send SYN ACK B
+    	# ACK C
+        (clientsocket, address) = (1, 1)  # change this to your code 
+        return (clientsocket, address)
     
     def close(self):   # fill in your code here 
         return 
 
     def send(self, buffer):
+    	# Create header
+    	# Send data
+    	# Start timer
+    	# If timeout, send same packet again
         bytessent = 0     # fill in your code here 
         return bytessent 
 
     def recv(self, nbytes):
+    	# Packets recv
+    	# Send right ACK
         bytesreceived = 0     # fill in your code here
         return bytesreceived 
-
-
-    
-
-
