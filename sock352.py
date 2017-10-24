9be644e3da4a6d3febb0292269b87d414f9c1dad
Benjamin Ker <kerbenjamin@gmail.com>
2017-10-23 22:58:31 -0400

    implementations
    init, constructor, partial connect

--- sock352.py
+++ sock352.py
@@ -8,25 +8,71 @@ import sys
 # define the UDP ports all messages are sent
 # and received from
 
-def init(UDPportTx, UDPportRx):   # initialize your UDP socket here 
-    pass 
-    
+def init(UDPportTx, UDPportRx):   # initialize your UDP socket here
+    # UDP Port
+    global RECV_PORT, SEND_PORT
+    RECV_PORT = UDPportTx
+    SEND_PORT = UDPportRx
+
+    # Header Default
+    global PKT_HEADER_DATA, HEADER_LEN, VERSION, OPT_PTR, PROTOCOL, SRC_PORT, DEST_PORT, WINDOW, CHECKSUM
+    PKT_HEADER_DATA = struct.Struct('!BBBBHHLLQQLL')
+    HEADER_LEN = sys.getsizeof(PKT_HEADER_DATA)
+    VERSION = 0x1
+    OPT_PTR = 0
+    PROTOCOL = 0
+    SRC_PORT = 0
+    DEST_PORT = 0
+    WINDOW = 0 # Unused for part 1
+    CHECKSUM = 0 # Unused for part 1
+
+    # Flags
+    global SYN, FIN, ACK, RES, OPT
+    SYN = 0b00000001
+    FIN = 0b00000010
+    ACK = 0b00000100
+    RES = 0b00001000
+    OPT = 0b00010000
+
 class socket:
     
     def __init__(self):  # fill in your code here 
-        return
+        self.sock = syssock.socket(syssock.AF_INET, syssock.SOCK_DGRAM)
     
     def bind(self, address):
         return 
 
     def connect(self, address):  # fill in your code here
-    	# Bind
+    	# Bind < NO
     	# Create the SYN header
     	# Send the SYN packet A
     	# Start timer
     	# recv SYN ACK B
     	# send ACK C
-    	# if there is error, send header again
+    	# If there is error, send header again
+
+
+        self.sock.connect(address)
+        seq_num = 0
+        ack_num = seq_num + 1
+        payload_len = 0
+
+        header = PKT_HEADER_DATA.pack(  VERSION,
+                                        SYN,
+                                        OPT_PTR,
+                                        PROTOCOL,
+                                        CHECKSUM,
+                                        SRC_PORT,
+                                        DEST_PORT,
+                                        seq_num,
+                                        ack_num,
+                                        WINDOW,
+                                        payload_len )
+
+        self.send(header)
+
+
+
         return 
     
     def listen(self, backlog):
@@ -54,4 +100,4 @@ class socket:
     	# Packets recv
     	# Send right ACK
         bytesreceived = 0     # fill in your code here
-        return bytesreceived 
+        return bytesreceived
